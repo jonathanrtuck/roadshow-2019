763b6204e739805bb94a9260c833caccb6c7ada5
Jonathan Tucker <jonathan.tucker1@pepsico.com>
2019-03-08 10:33:22 -0600

    created Page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import { Card, CardContent, Typography, withStyles } from '@material-ui/core';
 
 /**
  * @constant
@@ -7,9 +8,64 @@ import { Typography } from '@material-ui/core';
  * @param {object} props
  * @returns {ReactElement}
  */
-const Page = () => <Typography>profile…</Typography>;
+const Page = ({ classes, content, image, title }) => (
+  <Fragment>
+    <header
+      className={classes.header}
+      style={{ backgroundImage: `url(${image})` }}
+    />
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          align="center"
+          className={classes.title}
+          component="h1"
+          variant="h3"
+        >
+          {title}
+        </Typography>
+        {content}
+      </CardContent>
+    </Card>
+  </Fragment>
+);
+
+Page.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+  content: PropTypes.element.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 /**
  * @type {ReactElement}
  */
-export default Page;
+export default withStyles((theme) => ({
+  card: {
+    padding: theme.spacing.unit * 4,
+
+    [theme.breakpoints.up('md')]: {
+      margin: {
+        bottom: theme.spacing.unit * 4,
+        left: 'auto',
+        right: 'auto',
+        top: -theme.spacing.unit * 30,
+      },
+      width: theme.breakpoints.values.md,
+    },
+  },
+
+  header: {
+    background: {
+      position: 'center',
+      size: 'cover',
+    },
+    height: theme.spacing.unit * 60,
+  },
+
+  title: {
+    margin: {
+      bottom: theme.spacing.unit * 4,
+    },
+  },
+}))(Page);
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import hero from 'images/background.png';
+import Page from 'components/Page';
+import React, { Fragment } from 'react';
 import { Typography } from '@material-ui/core';
+
+/**
+ * @constant
+ * @type {ReactElement}
+ */
+const content = (
+  <Fragment>
+    <Typography gutterBottom variant="body1">
+      Suspendisse tempor dignissim urna, sed aliquet dui semper eget. In quam
+      nisl, pharetra id urna sit amet, porta sollicitudin dui. Phasellus id ante
+      et ipsum pharetra ullamcorper ut in tortor. Suspendisse gravida sed libero
+      id sodales. Donec tristique laoreet velit, quis ullamcorper nunc euismod
+      at. Cras blandit vulputate sagittis. Nunc in ligula at erat fermentum
+      elementum in sit amet libero. Integer et leo non lacus porttitor rhoncus
+      sed et eros. Donec eget maximus sapien. Pellentesque dapibus metus auctor
+      nisl venenatis lobortis. Sed pulvinar dignissim luctus. Nunc justo eros,
+      ultricies mattis sapien at, placerat fringilla dui.
+    </Typography>
+    <Typography variant="body1">
+      Praesent molestie odio lectus, et posuere mauris varius ac. Mauris
+      placerat tortor facilisis, pretium lectus nec, dictum nisl. Morbi non
+      condimentum ligula, eu imperdiet erat. Morbi interdum, justo vitae tempus
+      auctor, ipsum dui vehicula nisi, vel tincidunt metus risus vitae orci.
+      Praesent aliquet enim metus, vitae malesuada quam congue eu. Donec quis
+      scelerisque felis. Pellentesque pretium lacus et est tempor, ac lobortis
+      ipsum congue. Cras tempor augue vitae libero sagittis, interdum finibus
+      enim feugiat. Donec eget accumsan turpis, sit amet porttitor mauris.
+      Integer volutpat, ex at placerat varius, lectus libero sagittis tellus, ut
+      sodales ante libero sit amet purus. Mauris at gravida nibh, quis porta
+      arcu. Sed leo sem, euismod non malesuada eu, tempor quis mi.
+    </Typography>
+  </Fragment>
+);
 
 /**
  * @constant
@@ -7,7 +42,9 @@ import { Typography } from '@material-ui/core';
  * @param {object} props
  * @returns {ReactElement}
  */
-const Profile = () => <Typography>profile…</Typography>;
+const Profile = () => (
+  <Page content={content} image={hero} title="Lorem ipsum dolor sit amet" />
+);
 
 /**
  * @type {ReactElement}
